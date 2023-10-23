8569d8318e6c09ef1ab40dfbe2d6c0b9702d758f
Reza Sahraeian <bezhvin@gmail.com>
2023-10-23 22:55:52 +0200

    modifying

--- src/data_builder/mnist.py
+++ src/data_builder/mnist.py
@@ -3,6 +3,23 @@ import torchvision.datasets as dataset
 import torchvision.transforms as transforms
 import os
 
+class MNIST:
+    def __init__(self, root:str='./data/mnist'):
+        if not os.path.isdir(root):
+            os.makedirs(root)
+        transform = transforms.Compose([transforms.ToTensor()])
+        self.train_set = dataset.MNIST(root=root, train=True, transform=transform, download=True)
+        self.test_set = dataset.MNIST(root=root, train=False, transform=transform, download=True)
+
+    def subset_traindata(self, labels:list):
+        targets = self.train_set.targets
+        indeces = [i for i in range(len(targets)) if targets[i] in labels]
+        train_subset = torch.utils.data.Subset(self.train_set, indeces)
+        return train_subset
+
+
+
+
 def get_all_data(root:str='./data/mnist'):
     if not os.path.isdir(root):
         os.makedirs(root)
---
+++ src/models/linear_model.py
@@ -0,0 +1,4 @@
+import torch.optim as optim
+import torch.nn as nn
+from torch.utils.data import DataLoader
+import torch.nn.functional as F
